01122a52846d92b41a065f1866bb37fdf44ba77f
Md. Asif Iqbal <71099428+md-asifiqbal@users.noreply.github.com>
2026-04-02 20:47:28 +0600

    fixed

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -21,6 +21,12 @@ return Application::configure(basePath: dirname(__DIR__))
             HandleInertiaRequests::class,
             AddLinkHeadersForPreloadedAssets::class,
         ]);
+
+        $middleware->preventRequestForgery(
+            except: [
+                'iclock/*',
+            ]
+        );
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
