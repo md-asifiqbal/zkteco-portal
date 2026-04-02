95e79c3dd0e7563c417b2cf4bf255810ee3c9895
Md. Asif Iqbal <71099428+md-asifiqbal@users.noreply.github.com>
2026-04-03 01:06:12 +0600

    fixed

--- app/Models/PunchLog.php
+++ app/Models/PunchLog.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class PunchLog extends Model
 {
-    protected $connection = 'logdb';
+    protected $connection = 'logs_db';
 
     protected $guarded = [];
 }
