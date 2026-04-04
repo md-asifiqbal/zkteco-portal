e37a1305ed7ab4c2da32eb0ad5bf2cc45a62d615
Md. Asif Iqbal <71099428+md-asifiqbal@users.noreply.github.com>
2026-04-04 11:43:18 +0600

    fixed

---
+++ database/migrations/2026_04_04_053825_add_port_field_to_devices_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('devices', function (Blueprint $table) {
+            $table->integer('port')->default(4370)->after('ip_address');
+            $table->string('comm_key')->nullable()->after('port');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('devices', function (Blueprint $table) {
+            $table->dropColumn('port');
+            $table->dropColumn('comm_key');
+        });
+    }
+};
