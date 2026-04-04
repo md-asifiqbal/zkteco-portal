29b63386b5abefb2a00f9e228b7f1f8af41c075d
Md. Asif Iqbal <71099428+md-asifiqbal@users.noreply.github.com>
2026-04-04 11:51:38 +0600

    fixed

--- database/migrations/2026_04_04_053825_add_port_field_to_devices_table.php
+++ database/migrations/2026_04_04_053825_add_port_field_to_devices_table.php
@@ -16,7 +16,6 @@ return new class extends Migration
         Schema::table('devices', function (Blueprint $table) {
             $table->integer('port')->default(4370)->after('ip_address');
             $table->string('comm_key')->nullable()->after('port');
-            $table->boolean('is_cloud_supported')->default(false)->after('comm_key');
         });
     }
 
@@ -28,7 +27,6 @@ return new class extends Migration
         Schema::table('devices', function (Blueprint $table) {
             $table->dropColumn('port');
             $table->dropColumn('comm_key');
-            $table->dropColumn('is_cloud_supported');
         });
     }
 };
