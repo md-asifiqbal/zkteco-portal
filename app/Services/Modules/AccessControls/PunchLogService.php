e8820c8963900c9dd6e5785c9933923ed5cbc5b6
Md. Asif Iqbal <71099428+md-asifiqbal@users.noreply.github.com>
2026-04-14 17:58:21 +0600

    fixed api

--- app/Services/Modules/AccessControls/PunchLogService.php
+++ app/Services/Modules/AccessControls/PunchLogService.php
@@ -19,6 +19,14 @@ class PunchLogService
 
         $query = $this->model::query()->with('device')->where('tenant_id', tenant_id());
 
+        $query->when(isset($filters['user_id']), function ($q) use ($filters) {
+            $q->where('user_id', $filters['user_id']);
+        })->when(isset($filters['start_date']) && isset($filters['end_date']), function ($q) use ($filters) {
+            $q->whereBetween('timestamp', [$filters['start_date'], $filters['end_date']]);
+        })->when(isset($filters['source']), function ($q) use ($filters) {
+            $q->where('source', $filters['source']);
+        });
+
         if ($deviceId) {
             $query->where('device_id', $deviceId);
         }
