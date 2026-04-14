3afb6312ad6f796ef53163cba81dc1afd71295c3
Md. Asif Iqbal <71099428+md-asifiqbal@users.noreply.github.com>
2026-04-14 17:50:59 +0600

    fixed api

--- app/Http/Controllers/Api/V1/AccessControls/PunchLogController.php
+++ app/Http/Controllers/Api/V1/AccessControls/PunchLogController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Api\V1\AccessControls;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\Modules\AccessControls\PunchLogs\PunchLogResource;
+use App\Services\Applications\Api\ApiResponse;
 use App\Services\Modules\AccessControls\PunchLogService;
 use Illuminate\Http\Request;
 
@@ -12,6 +14,10 @@ class PunchLogController extends Controller
 
     public function getLogs(Request $request, ?string $deviceId = null)
     {
+        return $this->handleRequest(function () use ($request, $deviceId) {
+            $data = $this->punchLogService->getLogs($deviceId, $request->all());
 
+            return ApiResponse::success(PunchLogResource::collection($data));
+        });
     }
 }
